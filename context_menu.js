83835db7091c8c92587b23beb565c732f3f3bccc
Frank Jania <fjania@findings.com>
2012-09-18 15:43:05 -0400

    context menu clip

---
+++ context_menu.js
@@ -0,0 +1,25 @@
+// A generic onclick callback function.
+function handleClick(info, tab) {
+    var data = {
+        "content": info.selectionText,
+        "url": info.pageUrl,
+        "title": tab.title,
+        "clip-method": 'chrome-context-menu' 
+    };
+    console.log("posting: ", data);
+    var notification = showNotification('Posting Clip to Findings...');
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: config.findingsBaseURL + '/clips/enterclip/',
+        data: data,
+        success: function(data){ handleResponse(data, notification)},
+        error: function(jqXHR){ handleError(data, notification)}
+    });
+}
+
+var id = chrome.contextMenus.create({
+    "title": "Clip to Findings",
+    "contexts":['selection'],
+    "onclick": handleClick
+});
