3b7918308ea9932b0b2b39717a629131991487c4
Frank Jania <fjania@findings.com>
2012-10-26 14:57:29 -0400

    Setting the inline highlight status

--- src/options.js
+++ src/options.js
@@ -36,4 +36,11 @@ $(document).ready(function() {
     */
 
     $("#quick_clip_key").val('C');
+
+    $("#inline_highlighting_enabled").prop('checked', config.inlineHighlightingEnabled);
+
+    $("#inline_highlighting_enabled").change(function() {
+        config.inlineHighlightingEnabled = $("#inline_highlighting_enabled").prop("checked");
+        console.log("Inline Highlighting Endabled:", config.inlineHighlightingEnabled);
+    });
 });
